e5c44015e58beb865d204fac7e2ec7744cab2617
marcosvgalupo <mv.galupo@hotmail.com>
2024-05-06 13:42:22 -0300

    feat: add connection front-back

--- backend/src/main/java/com/elasticsearch/search/SearchApplication.java
+++ backend/src/main/java/com/elasticsearch/search/SearchApplication.java
@@ -2,12 +2,22 @@ package com.elasticsearch.search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class SearchApplication {
+public class SearchApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SearchApplication.class, args);
 	}
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/v1/**")
+				.allowedOrigins("http://localhost:5173")
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+				.allowCredentials(true)
+				.allowedHeaders("*");
+	}
 }
--- frontend/src/connection/api.ts
+++ frontend/src/connection/api.ts
@@ -5,7 +5,7 @@ const apiURL: string = 'http://localhost:8080/v1/search?query='
 
 function fetchApi(query: string){
     axios
-        .post(apiURL + query)
+        .get(apiURL + query)
         .then( res => console.log(res) )
         .catch( err => console.log(err) )
 }
